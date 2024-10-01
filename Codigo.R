49f54e55ffaa47c44252b63bf01ef66f043f665a
ro-canteros <rycanteros@gmail.com>
2024-10-01 10:15:17 -0300

    Comparacion de modelos de 3 var.

--- Codigo.R
+++ Codigo.R
@@ -1,36 +1,55 @@
+# ---Librerias---
 library(readr)
 library(tidyverse)
-datos <- read_csv("mushroom_cleaned.csv")
 
-colnames(datos) = c("cap_diameter","cap_shape", "gill_attachment", "gill_color", "stem_height", "stem_width", "stem_color", "season", "class")
 
-datos %>% mutate(cap_shape = as.factor(cap_shape), 
+# ---Datos---
+data_original <- read_delim("secondary_data.csv", 
+                            delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+hongos <- data_original %>% 
+  select(class, `cap-diameter`, `cap-shape`, `gill-attachment`, `gill-color`, 
+         `stem-height`, `stem-width`, `stem-color`, season) %>% 
+  mutate(class = ifelse(class == "e", 1, 0)) %>% 
+  replace_na(list(`gill-attachment` = "u"))
+
+colnames(hongos) = c("class", "cap_diameter","cap_shape", "gill_attachment", 
+                     "gill_color", "stem_height", "stem_width", "stem_color", 
+                     "season")
+
+hongos %>% mutate(cap_shape = as.factor(cap_shape), 
                  gill_attachment = as.factor(gill_attachment),
                  gill_color = as.factor(gill_color),
                  stem_color = as.factor(stem_color),
                  season = as.factor(season))
 
 
-unico1 = length(unique(datos$cap_shape))
+unico1 = length(unique(hongos$cap_shape))
 
-unico2 = length(unique(datos$gill_attachment))
+unico2 = length(unique(hongos$gill_attachment))
 
-unico3 = length(unique(datos$gill_color))
+unico3 = length(unique(hongos$gill_color))
 
-unico4 = length(unique(datos$stem_color))
+unico4 = length(unique(hongos$stem_color))
 
-unico5 = length(unique(datos$season))
+unico5 = length(unique(hongos$season))
 
 
-puntaje = as.data.frame(matrix(0, ncol = 4, nrow = 100))
+
+# ---Selección de variables---
+# Vemos cuántas y cuáles variables son significativas a modo de inicio; nos 
+# quedamos con aquellos modelos que tuvieron 3 variables significativas y los 
+# comparamos para ver cuál ajusta mejor.
+
+puntaje = as.data.frame(matrix(0, ncol = 4, nrow = 250))
 colnames(puntaje) = c("Puntaje", "Semilla", "Variables", "Categorias_faltantes")
 puntaje$Variables = ""
 
-for (i in 1:100) {
+for (i in 1:250) {
   
 set.seed(i)  
   
-datos_sample = datos[sample(54035,100),]
+datos_sample = hongos[sample(61069,250),]
 
 puntaje[i,2] = i
 
@@ -108,9 +127,77 @@ semilla = i
 
 
 puntaje
+table(datos_sample$class)
+
+# ---Comparación de los modelos---
+
+# Aparto los modelos de 3 variables:
+modelos <- puntaje[str_count(puntaje$Variables, "\\d+") == 3, ]
+
+
+modelo1 <- glm(class ~ stem_width + stem_color + season, family = binomial,
+               data = datos_sample)
+
+modelo2 <- glm(class ~ cap_shape + stem_color + season, family = binomial,
+               data = datos_sample)
+
+modelo3 <- glm(class ~ cap_diameter + stem_width + stem_color, family = binomial,
+               data = datos_sample)
+
+modelo4 <- glm(class ~ gill_attachment + stem_width + stem_color, family = binomial,
+               data = datos_sample)
+
+modelo5 <- glm(class ~ gill_color + stem_color + season, family = binomial,
+               data = datos_sample)
+
+modelo6 <- glm(class ~ gill_attachment + gill_color + stem_color, family = binomial,
+               data = datos_sample)
+
+modelo7 <- glm(class ~ cap_diameter + cap_shape + stem_color, family = binomial,
+               data = datos_sample)
+
+modelo8 <- glm(class ~ gill_color + stem_width + season, family = binomial,
+               data = datos_sample)
+
+modelo9 <- glm(class ~ cap_diameter + stem_color + season, family = binomial,
+               data = datos_sample)
+
+
+comparacion <- data.frame(
+  residual_d = c(modelo1$deviance, modelo2$deviance, modelo3$deviance,
+               modelo4$deviance, modelo5$deviance, modelo6$deviance,
+               modelo7$deviance, modelo8$deviance, modelo9$deviance),
+  df = c(modelo1$df.residual, modelo2$df.residual, modelo3$df.residual, 
+         modelo4$df.residual, modelo5$df.residual, modelo6$df.residual,
+         modelo7$df.residual, modelo8$df.residual, modelo9$df.residual)
+)
+
+
+combinaciones <- combn(1:9, 2)
+
+resultados <- data.frame(
+  dif_deviance = numeric(ncol(combinaciones)),
+  dif_df = numeric(ncol(combinaciones)),
+  prob_asoc = numeric(ncol(combinaciones)),
+  modelos = character(ncol(combinaciones))
+)
+
+
+for (k in 1:ncol(combinaciones)) {
+  i <- combinaciones[1, k]
+  j <- combinaciones[2, k]
+  
+  
+  resultados$dif_deviance[k] <- comparacion$residual_d[i] - comparacion$residual_d[j]
+  resultados$dif_df[k] <- abs(comparacion$df[i] - comparacion$df[j])
+  resultados$prob_asoc[k] <- pchisq(resultados$dif_deviance[k], resultados$dif_df[k], lower.tail = FALSE)
+  resultados$modelos[k] <- paste0(i, "-", j)
+}
 
 
 
-
-
+mejores <- resultados %>% 
+  filter(prob_asoc <= 0.06)
+# Los modelos 8 y 9 quedan descartados porque ajustan practicamente lo mismo y 
+# no son mejores que ningúno de los otros 7.
 
